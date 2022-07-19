b282fcfb81900836d7ce199625329fc9d44d162e
Saan Dev <saandev@saans-mbp.kpn>
2022-07-19 19:47:36 +0200

    added comments

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,14 @@ export default function Home() {
       <div >
         <br></br>
         <br></br>
+
+        // Example one: based on Smithsonian documentation
         <div className='explorer-wrapper'>
           <voyager-explorer root='https://3d-api.si.edu/content/document/341c96cd-f967-4540-8ed1-d3fc56d31f12/' document='document.json'></voyager-explorer>
         </div>
         <br></br>
+
+        // Example two: my code
         <div className='explorer-wrapper'>
           <voyager-explorer root='https://voyager-data.create.humanities.uva.nl/' document='tusk.svx.json'></voyager-explorer>
         </div>
